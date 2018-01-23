1e56141d37be9776e655bdf65c5cb02748b06bf9
Danielle Allan <30298565+awaywithdreams@users.noreply.github.com>
2018-01-23 11:39:42 -0500

    Add files via upload

---
+++ AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Payroll")>
+<Assembly: AssemblyDescription("An applicaion that records payroll for salaried and hourly employees and shows the total payroll")>
+<Assembly: AssemblyCompany("Niagaara University")>
+<Assembly: AssemblyProduct("Allan_Payroll_Project5")>
+<Assembly: AssemblyCopyright("Copyright ©  2016")>
+<Assembly: AssemblyTrademark("Danielle Allan")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("f5bcc8a0-d02f-490d-a6c2-4a1b6fa8f211")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
